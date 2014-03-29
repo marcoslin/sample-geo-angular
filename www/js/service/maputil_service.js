9b1b6dc953387ab10549cdefc344bea0b8bb17c1
Marcos Lin <linm.linmair@ebacimail.com>
2014-03-29 22:56:35 +0100

    Fixed the bug where option is not passed to getCurrentPosition in MapUtil

--- www/js/service/maputil_service.js
+++ www/js/service/maputil_service.js
@@ -6,13 +6,18 @@ angular.module("geoapp")
 
     // Get Position
     this.getPosition = function (enableHighAccuracyInput, timeout, maximumAge) {
+        $log.log("[MapUtil] enableHighAccuracyInput: ", enableHighAccuracyInput);
+        $log.log("[MapUtil] timeout: ", timeout);
+        $log.log("[MapUtil] maximumAge: ", maximumAge);
+        
         var d = $q.defer();
         
         options = {
             enableHighAccuracy: enableHighAccuracyInput || DefaultConfig.defaultEnableHighAccuracy,
-            timeout: timeout || DefaultConfig.defaultTimeout,
-            maximumAge: maximumAge || DefaultConfig.defaultMaximumAge
+            timeout: parseInt(timeout, 10) || DefaultConfig.defaultTimeout,
+            maximumAge: parseInt(maximumAge, 10) || DefaultConfig.defaultMaximumAge
         };
+        $log.log("[MapUtil] options: ", options);
         
         navigator.geolocation.getCurrentPosition(
             function (position) {
@@ -23,7 +28,8 @@ angular.module("geoapp")
                 var msg = error.message;
                 $window.alert(msg);
                 d.reject(msg);
-            }
+            },
+            options
         );
         
         return d.promise;
