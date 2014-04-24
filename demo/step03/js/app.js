4449acd9a74da8ff72af8c60e649fc11e30ce9ed
Marcos Lin <linm.linmair@ebacimail.com>
2014-04-24 14:58:13 +0200

    Added demo step03

---
+++ demo/step03/js/app.js
@@ -0,0 +1,83 @@
+(function() {
+    var app = angular.module("webapp", ['ngRoute']);
+
+    /**
+     * UNCHANGED
+     * Define Routes that binds View to a Controller
+     */
+    app.config(function ($routeProvider) {
+        $routeProvider
+        .when("/home", { templateUrl: "view/home.html", controller: "HomeCtrl" })
+        .otherwise({redirectTo: "/home"})
+    });
+
+    
+    /**
+     * Define a Service to be used by Controller
+     */
+    app.factory("ComplexData", function ($q, $timeout) {
+        var complexObj = function () {};
+        
+        complexObj.prototype.get = function (message, waitMSecs) {
+            var d = $q.defer();
+            
+            message = message || "Message from ComplexData";
+            waitMSecs = waitMSecs || 500;
+            
+            $timeout(function () {
+                d.resolve(message);
+            }, waitMSecs);
+
+            return d.promise;
+        };
+        
+        complexObj.prototype.error = function (errMessage) {
+            var d = $q.defer(),
+                waitMSecs = 500;
+            
+            errMessage = errMessage || "Error Message";
+            
+            $timeout(function () {
+                d.reject(errMessage);
+            }, waitMSecs);
+
+            return d.promise;
+        }
+        
+        
+        return new complexObj();
+        
+    });
+    
+
+    /**
+     * NEW: No longer injecting SimpleData
+     * Create controllers
+     */
+    app.controller("HomeCtrl", function ($scope, ComplexData) {
+        $scope.getData = function () {
+            $scope.messages = [];
+            $scope.errorMessage = "";
+            
+            ComplexData.get("Very first message").then(function (message) {
+                $scope.messages.push(message);
+                return ComplexData.get("Second message with a fast follow up", 1000);
+            }).then(function (message) {
+                $scope.messages.push(message);
+                return "A fast follow message without promise";
+            }).then(function (message) {
+                $scope.messages.push(message);
+                return ComplexData.get("The Final Message", 2000);
+            }).then(function (message) {
+                $scope.messages.push(message);
+                return ComplexData.error("An Expected Error");
+            }).catch(function (message) {
+                $scope.errorMessage = message;
+            });
+        };
+
+    });
+
+})();
+
+
