4cd34b8806308149d931b675713bfa6757462994
Marcos Lin <linm.linmair@ebacimail.com>
2014-04-24 12:37:41 +0200

    Added step01 of demo

---
+++ demo/step01/js/app.js
@@ -0,0 +1,38 @@
+(function() {
+    var app = angular.module("webapp", ['ngRoute']);
+
+    /**
+     * Define Routes that binds View to a Controller
+     */
+    app.config(function ($routeProvider) {
+        $routeProvider
+        .when("/home", { templateUrl: "view/home.html", controller: "HomeCtrl" })
+        .when("/one", { templateUrl: "view/page-one.html", controller: "OneCtrl" })
+        .otherwise({redirectTo: "/home"})
+    });
+
+    /**
+     * Define a Service to be used by Controller
+     */
+    app.service("SimpleData", function () {
+        var now = new Date();
+        this.state = now.getSeconds();
+        // this.state = 123;
+    });
+    
+    /**
+     * Create controllers
+     */
+    app.controller("HomeCtrl", function ($scope, SimpleData) {
+        $scope.title = "Home Page";
+        $scope.state = SimpleData.state;
+    });
+    
+    app.controller("OneCtrl", function ($scope, SimpleData) {
+        $scope.title = "This is Page ONE";
+        $scope.state = SimpleData.state;
+    });
+
+})();
+
+
